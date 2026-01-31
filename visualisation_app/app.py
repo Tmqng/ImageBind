4e983e27a28af8cae42fe9f0ad7f63cb48e1d0e6
Tmqng <minhquant0911@gmail.com>
2026-01-31 20:40:00 +0100

    add app.py

---
+++ visualisation_app/app.py
@@ -0,0 +1,19 @@
+import frontend
+
+from dash import Dash
+
+import os
+
+root_path = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+assets_path = os.path.join(root_path, ".assets")
+
+app = Dash(
+    __name__,
+    assets_folder=assets_path,
+    assets_url_path='my_assets'
+)
+
+app.layout = frontend.create_layout()
+
+if __name__ == '__main__':
+    app.run(debug=True)
